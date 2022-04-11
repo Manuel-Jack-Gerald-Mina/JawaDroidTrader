0424cd172d81df854910e1c17b448bc4b42102c0
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-04-11 14:44:35 -0500

    completed seeders and migrations

---
+++ usersSeeder.sql
@@ -0,0 +1,16 @@
+USE adlister_db;
+
+TRUNCATE users;
+
+INSERT INTO users (id, username, password)
+VALUES (1, 'adlister', 'codeup'),
+       (2, 'jawa', 'codeup'),
+       (3, 'mina', 'codeup'),
+       (4, 'jack', 'codeup'),
+       (5, 'geraldo', 'codeup'),
+       (6, 'alex', 'codeup'),
+       (7, 'cosmo', 'codeup'),
+       (8, 'orangeBanana', 'codeup'),
+       (9, 'PotatoPeeler', 'codeup'),
+       (10, 'BlackLagoon', 'codeup'),
+       (11, 'TwitterUser1', 'codeup');
