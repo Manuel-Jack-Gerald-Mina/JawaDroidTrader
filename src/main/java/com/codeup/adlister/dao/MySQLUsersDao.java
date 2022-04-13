93d378b530134b22119b52b54aa3b0d132c4c4a7
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-04-12 22:18:42 -0500

    usersDAO had same bug as AdsDAO, where we call a while(rs.next), to a method extractUser, which does a second rs.next. removed unnecessary next call

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -81,9 +81,6 @@ public class MySQLUsersDao implements Users {
     }
 
     private User extractUser(ResultSet rs) throws SQLException {
-        if (! rs.next()) {
-            return null;
-        }
         return new User(
                 rs.getLong("id"),
                 rs.getString("username"),
