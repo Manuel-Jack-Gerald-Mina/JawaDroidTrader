e810f3e00b5d9c75963bb906b1bf69ad54b13033
Gerald Montero <geraldmontero12@gmail.com>
2022-04-14 16:05:19 -0500

    adsInfo connected

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -59,7 +59,8 @@ public class MySQLAdsDao implements Ads {
                     rs.getLong("id"),
                     rs.getLong("user_id"),
                     rs.getString("title"),
-                    rs.getString("description")
+                    rs.getString("description"),
+                    rs.getDouble("price")
             );
 
 
@@ -72,7 +73,9 @@ public class MySQLAdsDao implements Ads {
                     rs.getLong("id"),
                     rs.getLong("user_id"),
                     rs.getString("title"),
-                    rs.getString("description")
+                    rs.getString("description"),
+                    rs.getDouble("price")
+
             ));
         }
         return ads;
